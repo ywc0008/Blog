97de0db2eac590480d7cf13117d6bae98e56bce2
wzz <ywc0008@naver.com>
2024-11-02 23:33:58 +0900

    Update LayoutFooter.tsx

--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -1,3 +1,9 @@
 export default function LayoutFooter() {
-  return <div>LayoutFooter</div>;
+  return (
+    <footer className="bg-gray-500 text-white p-4">
+      <div className="max-w-7xl mx-auto border-t border-gray-600 pt-4">
+        Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      </div>
+    </footer>
+  );
 }
