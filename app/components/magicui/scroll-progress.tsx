f5de821e4187498732e0cd18a4beac529f74b395
leewc <ywc0008@naver.com>
2025-02-10 17:45:13 +0900

    faet: shadcn(tailwind v4 canary) 설치

---
+++ app/components/magicui/scroll-progress.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { motion, type MotionProps, useScroll, useSpring } from "motion/react";
+import React from "react";
+interface ScrollProgressProps
+  extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
+
+export const ScrollProgress = React.forwardRef<
+  HTMLDivElement,
+  ScrollProgressProps
+>(({ className, ...props }, ref) => {
+  const { scrollYProgress } = useScroll();
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 50,
+    restDelta: 0.001,
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      className={cn(
+        "fixed inset-x-0 top-0 z-[1000] h-1 origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
+        className
+      )}
+      style={{
+        scaleX,
+      }}
+      {...props}
+    />
+  );
+});
+
+ScrollProgress.displayName = "ScrollProgress";
